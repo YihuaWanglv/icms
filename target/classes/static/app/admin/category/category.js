8f29df8f5a7b743eb0b926e794fff596c8415c25
yihuawanglv <wanglvyihua@gmail.com>
2017-03-05 14:11:15 +0800

    移除class文件

--- target/classes/static/app/admin/category/category.js
+++
@@ -1,9 +0,0 @@
-(function(angular) {
-  angular.module("category.controllers", []);
-  angular.module("category.services", []);
-  angular.module("category", ["ngResource", "category.controllers", "category.services"]
-  	, function($locationProvider) {
-    	$locationProvider.html5Mode(true);
-    }
-  );
-}(angular));
--- target/classes/static/app/admin/post/post-list.js
+++
@@ -1,7 +0,0 @@
-(function(angular) {
-	angular.module("post.controllers", []);
-	angular.module("post.services", []);
-	angular.module("post", ["ngResource", "post.controllers", "post.services"], function($locationProvider) {
-		$locationProvider.html5Mode(true);
-	});
-}(angular));
